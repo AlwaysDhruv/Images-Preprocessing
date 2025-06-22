37614e6219a1858f7741f42ff60aae5cf9a403db
AlwaysDhruv <sonavanebharat92@gmail.com>
2025-06-23 02:54:55 +0530

    Update

---
+++ source/main.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include "preprocess.h"
+
+using namespace std;
+
+void clearScreen();
+
+int main()
+{
+    Preprocess pre;
+    vector<string> folder_name;
+    pre.create_pixels_txt(folder_name);
+    return 0;
+}
