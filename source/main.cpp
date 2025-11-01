d88c2c1af58ba86aa84cd95426b47c7244492c6e
Dhruv Sonavane <sonavanebharat92@gmail.com>
2025-11-01 14:29:07 +0530

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -11,7 +11,8 @@ int main()
 {
     Preprocess pre;
     vector<string> folder_name;
-    pre.create_pixels_txt(folder_name); //return folde path
+    string path = pre.create_pixels_txt(folder_name); //return folde path
+    cout << "Path : " << path;
     pre.show(folder_name);
     return 0;
 }
